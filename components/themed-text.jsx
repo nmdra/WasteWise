210551be372529ec15e9dc52dc10aa85ba58d692
lksnjw <lk.snjw@gmail.com>
2025-10-04 10:00:00 +0530

    set up project
    Co-authored-by: nmdra <nimendradilshan11@gmail.com>
    Co-authored-by: Gayashan <hansagayashan7@gmail.com>
    Co-authored-by: DishaniS <dishanialuthwaththa@gmail.com>
    Co-authored-by: lksnjw <lk.snjw@gmail.com>

--- components/themed-text.jsx
+++ components/themed-text.jsx
@@ -1,20 +1,8 @@
-import { StyleSheet, Text, type TextProps } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 
 import { useThemeColor } from '@/hooks/use-theme-color';
 
-export type ThemedTextProps = TextProps & {
-  lightColor?: string;
-  darkColor?: string;
-  type?: 'default' | 'title' | 'defaultSemiBold' | 'subtitle' | 'link';
-};
-
-export function ThemedText({
-  style,
-  lightColor,
-  darkColor,
-  type = 'default',
-  ...rest
-}: ThemedTextProps) {
+export function ThemedText({ style, lightColor, darkColor, type = 'default', ...rest }) {
   const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
 
   return (
